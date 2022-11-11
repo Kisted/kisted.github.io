094ddd42bcddd86dec67b3fb6397679f8e0767a7
Kisted <kbisted@gmail.com>
2022-11-11 15:15:33 -0800

    Assignment 4: added js file and integrated into html file

---
+++ docs/fancifymytext.js
@@ -0,0 +1,17 @@
+function helloWorldAlert(){
+alert("Hello, world!");
+}
+
+function enlargeText(){
+document.getElementById("text_field").style.fontSize= "24px";
+}
+
+function changeText(){
+
+    var fancy_radio = document.getElementById("FancyShmancy");
+    var boring_radio = document.getElementById("BoringBetty");
+
+    if(fancy_radio.checked){
+
+    }
+}
